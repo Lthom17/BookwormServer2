292b9cbaa17a7fe43209f595dd06aac961f1959a
Lauren Thom <laurenthom17@gmail.com>
2024-05-03 21:28:51 -0500

    Add cors config

--- src/main/java/com/bookworm/config/SecurityConfig.java
+++ src/main/java/com/bookworm/config/SecurityConfig.java
@@ -1,10 +1,37 @@
 package com.bookworm.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+        .cors(c -> c.configurationSource(configurationSource()))
+                .authorizeHttpRequests(request -> request.anyRequest().permitAll());
+        return http.build();
+    }
     
 }
